23a51bab37ef0202732a6eb5ff1ef8583a26ca3a
wangjinsheng <wangjinsheng@t600.com.cn>
2017-03-09 11:08:28 +0800

    删除无效代码

--- classes/row0/CDRowZeroTVC.h
+++ classes/row0/CDRowZeroTVC.h
@@ -7,7 +7,6 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "PoemCell.h"
 
 @interface CDRowZeroTVC : UITableViewController
 
--- classes/row0/CDRowZeroTVC.m
+++ classes/row0/CDRowZeroTVC.m
@@ -12,11 +12,6 @@
 
 @interface CDRowZeroTVC ()
 
-//@property (nonatomic ,retain)NSArray * cellsArray;
-//@property (nonatomic ,retain)NSArray * dataArray;
-//
-//@property (nonatomic ,retain) UITableViewCell * tableViewCell;
-//@property (nonatomic ,retain) CDRowZeroCell * rowCell;
 
 // 从plist文件中加载的所有poem,返回所有的对象组成的数组
 @property (nonatomic, retain) NSArray *poem;
@@ -27,18 +22,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
 }
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-//    [self setDataOfVC];
 
 }
 - (void)didReceiveMemoryWarning {
@@ -58,49 +45,7 @@
     // Pass the selected object to the new view controller.
 }
 */
-- (void)setDataOfVC {
-    /*
-    self.cellsArray = @[@"Our Time",@"FU",@"No One",@"Vicious",@"Happy"];
-    self.dataArray = @[@"human",@"Slave To Love",@"Who's That Girl?",@"They're Not Waving",@"AQUARIUS"];
-    
-    CDRowZeroCell * cell = (CDRowZeroCell *)self.tableViewCell;
-//    self.rowCell = [CDRowZeroCell ];
-    */
-    
-    // 以下三步为OC标准代码,因为OC中不允许直接修该对象中结构体属性的成员的值,要通过中间的临时结构体变量
-//    CGRect frame = self.tableView.frame;
-//    frame.origin.y = 20;
-//    self.tableView.frame=frame;
-    
-    _poem = [NSMutableArray array];
-    // plist转成对象数组
-    [self plistToObjects];
-    
-}
-// plist转成对象数组
-- (void)plistToObjects
-{
-    
-    //从文件中读取plist文件的路径
-    NSString *path = [[NSBundle mainBundle] pathForResource:@"files" ofType:@"plist"];
-    
-     //初始化数据数组
-    NSArray *arrayWithDict  = [NSArray arrayWithContentsOfFile:path];
 
-    
-    NSLog(@"arrayWithDict----%@",arrayWithDict);
-    // 字典数组 _arrayWithDict
-    
-    // 方式2:类方法返回对象,参数只要一个字典数组即可
-    for (NSDictionary *dict in arrayWithDict) {
-        // 参数只要字典,这样一来,控制器就不用知道太多东西了
-        // Girl *girl = [[Girl alloc]initWithDict:dict];
-        PoemModel *model = [PoemModel poemWithDict:dict];
-//        [self.poem addObject:model];
-        NSLog(@"self.poem.-----%@",self.poem);
-
-    }
-}
 #pragma mark - Table view data source
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
@@ -108,35 +53,6 @@
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    /*
-    PoemCell *cell = (PoemCell *)[tableView dequeueReusableCellWithIdentifier:@"PoemCell"];
-    PoemModel *model = (self.poem)[indexPath.row];
-    cell.poemerLabel.text = model.poemer;
-    cell.poemNameLabel.text = model.poemName;
-    cell.poemContentLabel.text = model.poemContent;
-    
-    return cell;
-    */
-    /*
-    static NSString *CellTableIdentifer = @"PoemCell";
-    //tableView注册nib文件
-    static BOOL nibsRegistered = NO;
-    if(!nibsRegistered)
-    {
-        UINib *nib = [UINib nibWithNibName:@"PoemCell" bundle:nil];
-        [tableView registerNib:nib forCellReuseIdentifier:CellTableIdentifer];
-        nibsRegistered = YES;
-    }
-    
-    PoemCell * cell = [tableView dequeueReusableCellWithIdentifier:CellTableIdentifer];
-//    cell.textLabel.text = @"xxxxx";
-    PoemModel *model = (self.poem)[indexPath.row];
-    cell.poemerLabel.text = model.poemer;
-    cell.poemNameLabel.text = model.poemName;
-    cell.poemContentLabel.text = model.poemContent;
-    return cell;
-    */
-    
     static NSString *CellTableIdentifer = @"CDRowZeroCell";
     //tableView注册nib文件
     static BOOL nibsRegistered = NO;
