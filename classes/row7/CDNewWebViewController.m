868f136421f5578a83fc76607c21369217b6d7ed
wangjinsheng <wangjinsheng@t600.com.cn>
2017-05-02 14:06:15 +0800

    js调用iOS

--- classes/row7/CDNewWebViewController.m
+++ classes/row7/CDNewWebViewController.m
@@ -7,9 +7,19 @@
 //
 
 #import "CDNewWebViewController.h"
+#import <JavaScriptCore/JavaScriptCore.h>
 
 @interface CDNewWebViewController ()<UIWebViewDelegate>
 @property (weak, nonatomic) IBOutlet UIWebView *rootWebView;
+@property (weak, nonatomic) IBOutlet UIView *footerView;
+//刷新
+- (IBAction)refreshClick:(UIButton *)sender;
+//后退
+- (IBAction)goBackClick:(UIButton *)sender;
+
+//前进
+- (IBAction)goForwordClick:(UIButton *)sender;
+
 
 @end
 
@@ -41,18 +51,30 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    NSString *urlString = @"https://baidu.com.cn"; 
-    NSURL *url = [NSURL URLWithString:urlString];
+//    NSString *urlString = @"http://chongci.t600.com.cn/toLogin.jsp";
+//    NSURL *url = [NSURL URLWithString:urlString];
+//    //    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+//    //    [_webView loadRequest:request];
+//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+//    NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+//    [request setHTTPMethod:@"POST"];
+//    [request setHTTPBody:data];
+//    [_rootWebView loadData:data MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:url];
+    
+    
+    NSString *urlString = [[NSBundle mainBundle] pathForResource:@"Title" ofType:@"html"];
+    NSURL *url = [NSURL fileURLWithPath:urlString];
     //    NSURLRequest *request = [NSURLRequest requestWithURL:url];
     //    [_webView loadRequest:request];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+//    NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+    NSData *data = [[NSData alloc] initWithContentsOfFile:urlString];
     [request setHTTPMethod:@"POST"];
     [request setHTTPBody:data];
     [_rootWebView loadData:data MIMEType:@"text/html" textEncodingName:@"UTF-8" baseURL:url];
-    
+
 }
-#pragma mark - UIWebViewDelegate 
+#pragma mark - UIWebViewDelegate
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
       NSLog(@"ViewController:shouldStartLoadWithRequest-----%@",[NSString stringWithFormat:@"%@",request.URL.absoluteString]);
     return YES;
@@ -61,10 +83,64 @@
     
 }
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
+//    __block CDNewWebViewController* blockself = self;
+    JSContext *jsContext = nil;
+    if (!jsContext){
+        jsContext = (JSContext *)[_rootWebView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    }
+    jsContext[@"sendUrlLogin"] = ^(){
+        //点击登录
+        NSLog(@"sendUrlLogin");
+        [JSContext currentContext];
+    };
+    jsContext[@"sendUrlnoLogin"] = ^(){
+        //点击游客登录
+        NSLog(@"sendUrlnoLogin");
+        [JSContext currentContext];
+    };
+    jsContext[@"sendMessage1"] = ^(){
+        //点击游客登录
+        NSLog(@"sendMessage1");
+        [JSContext currentContext];
+    };
+    jsContext[@"loginWithoutAccount"] = ^(){
+        //点击游客登录
+        NSLog(@"loginWithoutAccount");
+        [JSContext currentContext];
+    };
     
+    jsContext[@"sendMessage3"] = ^(){
+        //点击游客登录
+        NSLog(@"sendMessage3");
+        [JSContext currentContext];
+    };
+    jsContext[@"sendMessage22"] = ^(){
+        //点击游客登录
+        NSLog(@"sendMessage22");
+        [JSContext currentContext];
+    };
+//    jsContext[@"sendMessage22"] = ^(NSString *data){
+//        //点击游客登录
+//        NSLog(@"sendMessage22======%@",data);
+//        [JSContext currentContext];
+//    };
+    jsContext[@"login"] = ^(){
+        //点击游客登录
+        NSLog(@"login 用户登录");
+        [JSContext currentContext];
+    };
 }
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
     
 }
 
+- (IBAction)refreshClick:(UIButton *)sender {
+    [_rootWebView reload];
+}
+- (IBAction)goBackClick:(UIButton *)sender {
+    [_rootWebView goBack];
+}
+- (IBAction)goForwordClick:(UIButton *)sender {
+    [_rootWebView goForward];
+}
 @end
